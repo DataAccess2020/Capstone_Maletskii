63ab3f137459b964a4996d33300933ff9345ca56
Vadim Maletskii <wiscain@yandex.ru>
2022-12-23 02:02:48 +0100

    Extracts albums

---
+++ Scripts/02-Extracting_albums_spotifyr.R
@@ -0,0 +1,9 @@
+gone.fludd = get_artist_albums(
+  id = "0ohUvVskERzK18bvWXFEqi",
+  include_groups = c("album"),
+  market = NULL,
+  limit = 50,
+  offset = 0,
+  authorization = token,
+  include_meta_info = FALSE
+)
